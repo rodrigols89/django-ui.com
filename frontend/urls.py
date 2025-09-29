86450d1c45017117bcf5fef6a5500dcae058503c
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-09-29 06:23:30 -0300

    Add 'Tailwind Spacing' studies

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, spacing
 
 urlpatterns = [
     path('', home, name='home'),
+    path(route="tailwind/spacing/", view=spacing, name="spacing"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     if request.method == 'GET':
         return render(request, 'pages/home.html')
+
+def spacing(request):
+    if request.method == 'GET':
+        return render(request, 'pages/tailwind/spacing.html')
