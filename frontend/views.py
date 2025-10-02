791a2f92596aa22cfef35e30b1f552ad0c555e62
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-10-02 16:41:58 -0300

    Add 'Core Concepts' examples

--- frontend/views.py
+++ frontend/views.py
@@ -24,3 +24,7 @@ def typography(request):
 def borders(request):
     if request.method == 'GET':
         return render(request, 'pages/tailwind/borders.html')
+
+def core_concepts(request):
+    if request.method == 'GET':
+        return render(request, 'pages/tailwind/core-concepts.html')
