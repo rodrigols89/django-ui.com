1eb75a82d5487ac4b356f5b9f34d41a70d1522df
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-10-07 20:21:05 -0300

    Update project to GitHub Pages using static files

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django_distill import distill_path
+from frontend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
+
+    # Home
+    distill_path('', views.home, name='home', distill_func=lambda: None),
+
+    # Páginas Tailwind
+    distill_path('tailwind/core-concepts/', views.core_concepts, name='core-concepts', distill_func=lambda: None),
+    distill_path('tailwind/spacing/', views.spacing, name='spacing', distill_func=lambda: None),
+    distill_path('tailwind/sizing/', views.sizing, name='sizing', distill_func=lambda: None),
+    distill_path('tailwind/layout/', views.layout, name='layout', distill_func=lambda: None),
+    distill_path('tailwind/typography/', views.typography, name='typography', distill_func=lambda: None),
+    distill_path('tailwind/borders/', views.borders, name='borders', distill_func=lambda: None),
 ]
